63f5040cf5e4028aa9a8359165215a1694965e59
Eku <jjiang127@connect.hkust-gz.edu.cn>
2024-03-07 20:34:32 +0800

    update my run docker

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker run -it --name test1 --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home:/home eku127/3dgs:no_vis /bin/zsh
+MESA_GL_VERSION_OVERRIDE=4.5
 
-export MESA_GL_VERSION_OVERRIDE=4.5
+docker run -it --name test1 --gpus all -e DISPLAY=$DISPLAY -e MESA_GL_VERSION_OVERRIDE=${MESA_GL_VERSION_OVERRIDE} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home:/home eku127/3dgs:no_vis /bin/zsh
+
+# export MESA_GL_VERSION_OVERRIDE=4.5
 
 echo "Please "
